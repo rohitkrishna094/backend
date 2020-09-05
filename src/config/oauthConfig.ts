a36ddeea7d1b46759af988e0da08a03f6cd8ed76
rohitkrishna094 <rohitkrishna094@gmail.com>
2020-09-04 22:38:49 -0400

    added baseurl

--- src/config/oauthConfig.ts
+++ src/config/oauthConfig.ts
@@ -1,11 +1,11 @@
 export const GITHUB = {
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: '/auth/github/callback'
+  callbackURL: `${process.env.BASE_URL}/auth/github/callback`
 };
 
 export const GOOGLE = {
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: `${process.env.BASE_URL}/auth/google/callback`
 };
